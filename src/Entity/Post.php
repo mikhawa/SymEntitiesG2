363f7127583102fb33eb817d6922e557cfdf06a2
WebDevCF2m <gitweb@cf2m.be>
2024-09-10 14:20:02 +0200

    php bin/console make:entity Post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
@@ -13,8 +14,83 @@ class Post
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 160)]
+    private ?string $postTitle = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $postText = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $postDateCreated = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $postDatePublished = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $postPublished = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPostTitle(): ?string
+    {
+        return $this->postTitle;
+    }
+
+    public function setPostTitle(string $postTitle): static
+    {
+        $this->postTitle = $postTitle;
+
+        return $this;
+    }
+
+    public function getPostText(): ?string
+    {
+        return $this->postText;
+    }
+
+    public function setPostText(string $postText): static
+    {
+        $this->postText = $postText;
+
+        return $this;
+    }
+
+    public function getPostDateCreated(): ?\DateTimeInterface
+    {
+        return $this->postDateCreated;
+    }
+
+    public function setPostDateCreated(?\DateTimeInterface $postDateCreated): static
+    {
+        $this->postDateCreated = $postDateCreated;
+
+        return $this;
+    }
+
+    public function getPostDatePublished(): ?\DateTimeInterface
+    {
+        return $this->postDatePublished;
+    }
+
+    public function setPostDatePublished(?\DateTimeInterface $postDatePublished): static
+    {
+        $this->postDatePublished = $postDatePublished;
+
+        return $this;
+    }
+
+    public function isPostPublished(): ?bool
+    {
+        return $this->postPublished;
+    }
+
+    public function setPostPublished(?bool $postPublished): static
+    {
+        $this->postPublished = $postPublished;
+
+        return $this;
     }
 }
