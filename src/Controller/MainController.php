b669fdb3bf68841c15854d26c1e304365f110dd8
mikhawa <michaeljpitz@gmail.com>
2024-09-12 12:00:20 +0200

    Controller main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $posts = $em->getRepository(Post::class)->findBy([], ['postDateCreated' => 'DESC']);
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'posts' => $posts,
         ]);
     }
 }
